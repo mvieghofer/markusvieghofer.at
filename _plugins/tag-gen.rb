f6692909b68ad2c2626d59d03e77d18d6b9171e9
Markus Vieghofer <markus.vieghofer@gmail.com>
2016-09-12 17:34:44 +0200

    added tag index

--- _plugins/tag-gen.rb
+++ _plugins/tag-gen.rb
@@ -1,37 +1,61 @@
+require "set"
 module Jekyll
 
-  class TagIndex < Page
-    def initialize(site, base, dir, tag)
-      @site = site
-      @base = base
-      @dir = dir
-      @name = 'index.html'
+    class TagPage < Page
+        def initialize(site, base, dir, tag)
+            @site = site
+            @base = base
+            @dir = dir
+            @name = 'index.html'
 
-      self.process(@name)
-      self.read_yaml(File.join(base, '_layouts'), 'tag_index.html')
-      self.data['tag'] = tag
-      self.data['title'] = "Posts Tagged &ldquo;"+tag+"&rdquo;"
-    end
-  end
-
-  class TagGenerator < Generator
-    safe true
-    
-    def generate(site)
-      if site.layouts.key? 'tag_index'
-        dir = 'tag'
-        site.tags.keys.each do |tag|
-          write_tag_index(site, File.join(dir, tag), tag)
+            self.process(@name)
+            self.read_yaml(File.join(base, '_layouts'), 'tag.html')
+            self.data['tag'] = tag
+            self.data['title'] = "Posts Tagged &ldquo;"+tag+"&rdquo;"
         end
-      end
     end
 
-    def write_tag_index(site, dir, tag)
-      index = TagIndex.new(site, site.source, dir, tag)
-      index.render(site.layouts, site.site_payload)
-      index.write(site.dest)
-      site.pages << index
+    class TagIndex < Page
+        def initialize(site, base, dir, tags)
+            @site = site
+            @base = base
+            @dir = dir
+            @name = 'index.html'
+
+            self.process(@name)
+            self.read_yaml(File.join(base, '_layouts'), 'tag_index.html')
+            self.data['tags'] = tags
+        end
     end
-  end
+
+    class TagGenerator < Generator
+        safe true
+
+        def generate(site)
+            if site.layouts.key? 'tag_index'
+                dir = 'tag'
+                allTags = Set.new
+                site.tags.keys.each do |tag|
+                    write_tag_page(site, File.join(dir, tag), tag)
+                    allTags.add(tag)
+                end
+                write_tag_index(site, dir, allTags)
+            end
+        end
+
+        def write_tag_page(site, dir, tag)
+            tagPage = TagPage.new(site, site.source, dir, tag)
+            tagPage.render(site.layouts, site.site_payload)
+            tagPage.write(site.dest)
+            site.pages << tagPage
+        end
+
+        def write_tag_index(site, dir, allTags)
+            tagIndex = TagIndex.new(site, site.source, dir, allTags)
+            tagIndex.render(site.layouts, site.site_payload)
+            tagIndex.write(site.dest)
+            site.pages << tagIndex
+        end
+    end
 
 end
